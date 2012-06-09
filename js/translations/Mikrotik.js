46540f6f0592124c33459b4261c461114dc4fb1c
dvdwalt <dvdwalt@4d75342d-bac5-42b9-a107-79493a2c96c2>
2012-06-09 04:45:47 +0000

    Add Italian Language
    git-svn-id: svn://svn.code.sf.net/p/hotcakes/code/trunk/yfi_cake/setup/coova_json@596 4d75342d-bac5-42b9-a107-79493a2c96c2

--- js/translations/Mikrotik.js
+++ js/translations/Mikrotik.js
@@ -11,321 +11,366 @@ if(!dojo._hasResource["translations.Mikrotik"]){ //_hasResource checks added by 
             'es_ES' : 'Nombre Usuario',
             'af_ZA' : 'Gebruikersnaam',
             'fr_FR' : 'Nom',
-  	  	    'ms_MY' : 'Username',
-		    'nl_NL' : 'Gebruikersnaam',
-            'id_ID' : 'Username'
+  	    'ms_MY' : 'Usernameaa',
+	    'nl_NL' : 'Gebruikersnaam',
+            'id_ID' : 'Username',
+	    'it_IT' : 'Nome Utente'
         },
         'Password': { 
             'es_ES' : 'Contraseña',
             'af_ZA' : 'Wagwoord',
             'fr_FR' : 'Mot de passe',
-  	  	    'ms_MY' : 'Kata Sandi',
-		    'nl_NL' : 'Wachtwoord',
-            'id_ID' : 'Kata Sandi'
+  	    'ms_MY' : 'Kata Sandi',
+	    'nl_NL' : 'Wachtwoord',
+            'id_ID' : 'Kata Sandi',
+	    'it_IT' : 'Password'
         },
         'Language': { 
             'es_ES' : 'Idioma',
             'af_ZA' : 'Taalvoorkeur',
             'fr_FR' : 'Langue',
-  	  	    'ms_MY' : 'Bahasa',
-		    'nl_NL' : 'Taal',
-            'id_ID' : 'Bahasa'
+  	    'ms_MY' : 'Bahasa',
+	    'nl_NL' : 'Taal',
+            'id_ID' : 'Bahasa',
+	    'it_IT' : 'Lingua'
         },
         'OK': { 
             'es_ES' : 'OK',
             'af_ZA' : 'OK',
             'fr_FR' : 'OK',
-  	  	    'ms_MY' : 'OK',
-		    'nl_NL' : 'OK',
-            'id_ID' : 'OK'
+  	    'ms_MY' : 'OK',
+	    'nl_NL' : 'OK',
+            'id_ID' : 'OK',
+	    'it_IT' : 'OK'
         },
         'Authentication Failed' :{ 
             'es_ES' : 'Falló la Autenticación',
             'af_ZA' : 'Geldigvastelling het gevaal',
             'fr_FR' : 'Authentification échouée',
-  	  	    'ms_MY' : 'Authentikasi gagal',
-		    'nl_NL' : 'Aanmelden mislukt',
-            'id_ID' : 'Authentikasi gagal'
+  	    'ms_MY' : 'Authentikasi gagal',
+	    'nl_NL' : 'Aanmelden mislukt',
+            'id_ID' : 'Authentikasi gagal',
+	    'it_IT' : 'Autenticazione fallita'
         },
         'Working - Please wait' : {
             'af_ZA' : 'Besig met versoek - net so oomblik',
             'fr_FR' : 'Travail - attendez svp',
-  	  	    'ms_MY' : 'Sedang bekerja - Silahkan tunggu',
-		    'nl_NL' : 'Bezig - een moment...',
-            'id_ID' : 'Sedang bekerja - Silahkan tunggu'
+  	    'ms_MY' : 'Sedang bekerja - Silahkan tunggu',
+	    'nl_NL' : 'Bezig - een moment...',
+            'id_ID' : 'Sedang bekerja - Silahkan tunggu',
+	    'it_IT' : 'Si prega di attendere...'
         },
         'Not a Hotspot' : {
             'af_ZA' : 'Toegang nie deur warmkol nie',
             'fr_FR' : 'Pas un point névralgique',
-  	  	    'ms_MY' : 'Bukan hotspot',
-		    'nl_NL' : 'Geen hotspot',
-            'id_ID' : 'Bukan hotspot'
+  	    'ms_MY' : 'Bukan hotspot',
+	    'nl_NL' : 'Geen hotspot',
+            'id_ID' : 'Bukan hotspot',
+	    'it_IT' : 'Non è un HotSpot'
         },
         'Feedback in pop-up': {
             'af_ZA' : 'Terugvoer in nuwe venster',
             'fr_FR' : 'Rétroaction dans automatique',
-  	  	    'ms_MY' : 'Feed back di Pop Up',
-		    'nl_NL' : 'Feedback in pop-up',
-            'id_ID' : 'Feed back di Pop Up'
+  	    'ms_MY' : 'Feed back di Pop Up',
+	    'nl_NL' : 'Feedback in pop-up',
+            'id_ID' : 'Feed back di Pop Up',
+	    'it_IT' : 'Statistiche in PopUp'
         },
         'Usage': {
             'af_ZA' : 'Gebruik',
             'fr_FR' : 'Utilisation',
-  	  	    'ms_MY' : 'Pemakaian',
-		    'nl_NL' : 'Verbruik',
-            'id_ID' : 'Pemakaian'
+  	    'ms_MY' : 'Pemakaian',
+	    'nl_NL' : 'Verbruik',
+            'id_ID' : 'Pemakaian',
+	    'it_IT' : 'Utilizzo'
         },
         'Stats': {
             'af_ZA' : 'Statistiek',
             'fr_FR' : 'Stat',
-  	  	    'ms_MY' : 'Statistik',
-		    'nl_NL' : 'Statistieken',
-            'id_ID' : 'Statistik'
+  	    'ms_MY' : 'Statistik',
+	    'nl_NL' : 'Statistieken',
+            'id_ID' : 'Statistik',
+	    'it_IT' : 'Statistiche'
         },
         'Time Used': {
             'af_ZA' : 'Tydsgebruik',
             'fr_FR' : 'Le temps a employé',
-  	  	    'ms_MY' : 'Waktu terpakai',
-		    'nl_NL' : 'Verbruikte tijd',
-            'id_ID' : 'Waktu terpakai'
+  	    'ms_MY' : 'Waktu terpakai',
+	    'nl_NL' : 'Verbruikte tijd',
+            'id_ID' : 'Waktu terpakai',
+	    'it_IT' : 'Tempo utilizzato'
         },
         'Data Used': {
             'af_ZA' : 'Datagebruik',
             'fr_FR' : 'Données utilisées',
-  	  	    'ms_MY' : 'Data terpakai',
-		    'nl_NL' : 'Verbruikte data',
-            'id_ID' : 'Data terpakai'
+  	    'ms_MY' : 'Data terpakai',
+	    'nl_NL' : 'Verbruikte data',
+            'id_ID' : 'Data terpakai',
+	    'it_IT' : 'Dati utilizzati'
         },
         'Used': {
             'af_ZA' : 'Opgebruik',
             'fr_FR' : 'Utilisé',
-  	  	    'ms_MY' : 'Terpakai',
-		    'nl_NL' : 'Verbruikt',
-            'id_ID' : 'Terpakai'
+  	    'ms_MY' : 'Terpakai',
+	    'nl_NL' : 'Verbruikt',
+            'id_ID' : 'Terpakai',
+	    'it_IT' : 'Usato'
         },
         'Available': {
             'af_ZA' : 'Beskikbaar',
             'fr_FR' : 'Disponible',
-  	  	    'ms_MY' : 'Tersedia',
-		    'nl_NL' : 'Beschikbaar',
-            'id_ID' : 'Tersedia'
+  	    'ms_MY' : 'Tersedia',
+	    'nl_NL' : 'Beschikbaar',
+            'id_ID' : 'Tersedia',
+	    'it_IT' : 'Disponibile'
         },
         'Are you sure you want to disconnect': {
             'af_ZA' : 'Wil jy regtig ontkoppel',
             'fr_FR' : 'Êtes vous sure que vous voulez déconnecter',
-  	  	    'ms_MY' : 'Apa anda yakin anda akan memutuskan koneksi',
-		    'nl_NL' : 'Weet u zeker dat u de verbinding wilt verbreken',
-            'id_ID' : 'Apa anda yakin anda akan memutuskan koneksi'
+  	    'ms_MY' : 'Apa anda yakin anda akan memutuskan koneksi',
+	    'nl_NL' : 'Weet u zeker dat u de verbinding wilt verbreken',
+            'id_ID' : 'Apa anda yakin anda akan memutuskan koneksi',
+	    'it_IT' : 'Sei sicuro di volerti disconnettere?'
         },
         'Connected': {
             'af_ZA' : 'Gekoppel',
             'fr_FR' : 'Relié',
-  	  	    'ms_MY' : 'Tersambung',
-		    'nl_NL' : 'Verbonden',
-            'id_ID' : 'Tersambung'
+  	    'ms_MY' : 'Tersambung',
+	    'nl_NL' : 'Verbonden',
+            'id_ID' : 'Tersambung',
+	    'it_IT' : 'Connesso'
         },
         'Session ID': {
             'af_ZA' : 'Sessie ID',
             'fr_FR' : 'Identification de session',
-  	  	    'ms_MY' : 'ID sesi',
-		    'nl_NL' : 'Sessie-ID',
-            'id_ID' : 'ID sesi'
+  	    'ms_MY' : 'ID sesi',
+	    'nl_NL' : 'Sessie-ID',
+            'id_ID' : 'ID sesi',
+	    'it_IT' : 'ID Sessione'
         },
         'Not available': {
             'af_ZA' : 'Nie beskikbaar',
             'fr_FR' : 'Non disponible',
-  	  	    'ms_MY' : 'Tidak tersedia',
-		    'nl_NL' : 'Niet beschikbaar',
-            'id_ID' : 'Tidak tersedia'
+  	    'ms_MY' : 'Tidak tersedia',
+	    'nl_NL' : 'Niet beschikbaar',
+            'id_ID' : 'Tidak tersedia',
+	    'it_IT' : 'Non disponibile'
         },
         'Max Session Time': {
             'af_ZA' : 'Maksimum sessie tyd',
             'fr_FR' : 'Temps maximum de session',
-  	  	    'ms_MY' : 'Sesi waktu maksimal',
-		    'nl_NL' : 'Max. sessieduur',
-            'id_ID' : 'Sesi waktu maksimal'
+  	    'ms_MY' : 'Sesi waktu maksimal',
+	    'nl_NL' : 'Max. sessieduur',
+            'id_ID' : 'Sesi waktu maksimal',
+	    'it_IT' : 'Durata massima sessione'
         },
         'Max Session Time': {
             'af_ZA' : 'Maksimum sessie tyd',
             'fr_FR' : 'Temps maximum de session',
-  	  	    'ms_MY' : 'Sesi waktu maksimal',
-		    'nl_NL' : 'Max. sessieduur',
-            'id_ID' : 'Sesi waktu maksimal'
+  	    'ms_MY' : 'Sesi waktu maksimal',
+	    'nl_NL' : 'Max. sessieduur',
+            'id_ID' : 'Sesi waktu maksimal',
+	    'it_IT' : 'Durata massima sessione'
         },
         'unlimited': {
             'af_ZA' : 'onbeperk',
             'fr_FR' : 'illimité',
-  	  	    'ms_MY' : 'Tak terbatas',
-		    'nl_NL' : 'onbeperkt',
-            'id_ID' : 'Tak terbatas'
+  	    'ms_MY' : 'Tak terbatas',
+	    'nl_NL' : 'onbeperkt',
+            'id_ID' : 'Tak terbatas',
+	    'it_IT' : 'illimitato'
         },
         'Max Idle Time': {
             'af_ZA' : 'Maksimum dooietyd',
             'fr_FR' : 'Temps à vide maximum',
-  	  	    'ms_MY' : 'Waktu Maximum idle',
-		    'nl_NL' : 'Max. idle-duur',
-            'id_ID' : 'Waktu Maximum idle'
+  	    'ms_MY' : 'Waktu Maximum idle',
+	    'nl_NL' : 'Max. idle-duur',
+            'id_ID' : 'Waktu Maximum idle',
+	    'it_IT' : 'Tempo massimo di inattività'
         },
         'Start Time':{
             'af_ZA' : 'Begintyd',
             'fr_FR' : 'Heure de départ',
-  	  	    'ms_MY' : 'Waktu Mulai',
-		    'nl_NL' : 'Begintijd',
-            'id_ID' : 'Waktu Mulai'
+	    'ms_MY' : 'Waktu Mulai',
+	    'nl_NL' : 'Begintijd',
+            'id_ID' : 'Waktu Mulai',
+	    'it_IT' : 'Ora inizio'
         },
         'Session Time':{
             'af_ZA' : 'Sessietyd',
             'fr_FR' : 'Temps de session',
-  	  	    'ms_MY' : 'Sesi',
-		    'nl_NL' : 'Sessieduur',
-            'id_ID' : 'Sesi'
+  	    'ms_MY' : 'Sesi',
+	    'nl_NL' : 'Sessieduur',
+            'id_ID' : 'Sesi',
+	    'it_IT' : 'Durata sessione'
         },
         'Idle Time':{
             'af_ZA' : 'Dooietyd',
             'fr_FR' : 'Temps à vide',
-  	  	    'ms_MY' : 'Waktu Idle',
-		    'nl_NL' : 'Idle-duur',
-            'id_ID' : 'Waktu Idle'
+	    'ms_MY' : 'Waktu Idle',
+	    'nl_NL' : 'Idle-duur',
+            'id_ID' : 'Waktu Idle',
+	    'it_IT' : 'Tempo di inattività'
         },
         'Downloaded' :{
             'af_ZA' : 'Afgelaai',
             'fr_FR' : 'Téléchargé',
-  	  	    'ms_MY' : 'Di Download',
-		    'nl_NL' : 'Gedownload',
-            'id_ID' : 'Di Download'
+  	    'ms_MY' : 'Di Download',
+	    'nl_NL' : 'Gedownload',
+            'id_ID' : 'Di Download',
+	    'it_IT' : 'Scaricato'
         },
         'Uploaded' :{
             'af_ZA' : 'Opgelaai',
             'fr_FR' : 'Téléchargé',
-  	  	    'ms_MY' : 'Di Upload',
-		    'nl_NL' : 'Geupload',
-            'id_ID' : 'Di Upload'
+  	    'ms_MY' : 'Di Upload',
+	    'nl_NL' : 'Geupload',
+            'id_ID' : 'Di Upload',
+	    'it_IT' : 'Inviato'
         },
         'Original URL' :{
             'af_ZA' : 'Oorspronklike URL',
             'fr_FR' : 'URL original',
-  	  	    'ms_MY' : 'URL Asli',
-		    'nl_NL' : 'Oorspronkelijke URL',
-            'id_ID' : 'URL asli'
+	    'ms_MY' : 'URL Asli',
+	    'nl_NL' : 'Oorspronkelijke URL',
+            'id_ID' : 'URL asli',
+	    'it_IT' : 'URL Originale'
         },
         'logout' :{
             'af_ZA' : 'ontkoppel',
             'fr_FR' : 'déconnexion',
-  	  	    'ms_MY' : 'Keluar',
-		    'nl_NL' : 'Uitloggen',
-            'id_ID' : 'Keluar'
+  	    'ms_MY' : 'Keluar',
+	    'nl_NL' : 'Uitloggen',
+            'id_ID' : 'Keluar',
+	    'it_IT' : 'Disconnetti'
         },
         'Connect':{
             'af_ZA' : 'Koppel',
             'fr_FR' : 'Releiz',
-  	  	    'ms_MY' : 'Sambung',
-		    'nl_NL' : 'Verbinden',
-            'id_ID' : 'Sambung'
+  	    'ms_MY' : 'Sambung',
+	    'nl_NL' : 'Verbinden',
+            'id_ID' : 'Sambung',
+	    'it_IT' : 'Connetti'
         },
         'Disconnect' : {
             'af_ZA' : 'Ontkoppel',
             'fr_FR' : 'Débranchement',
-  	  	    'ms_MY' : 'Putuskan',
-		    'nl_NL' : 'Verbinding verbreken',
-            'id_ID' : 'Putuskan'
+  	    'ms_MY' : 'Putuskan',
+	    'nl_NL' : 'Verbinding verbreken',
+            'id_ID' : 'Putuskan',
+	    'it_IT' : 'Disconnetti'
         },
         'Loggin in - Please wait': {
             'af_ZA' : 'Teken in - Wag asseblief',
             'fr_FR' : 'Loggin dedans - attendez svp',
-  	  	    'ms_MY' : 'Login - Silahkan tunggu',
-		    'nl_NL' : 'Inloggen - een moment...',
-            'id_ID' : 'Login - Silahkan tunggu'
+  	    'ms_MY' : 'Login - Silahkan tunggu',
+	    'nl_NL' : 'Inloggen - een moment...',
+            'id_ID' : 'Login - Silahkan tunggu',
+	    'it_IT' : 'Connessione - Attendere prego...'
         },
        'Bytes': {
             'af_ZA' : 'Grepe',
             'fr_FR' : 'Octets',
-  	  	    'ms_MY' : 'Bytes',
-		    'nl_NL' : 'Bytes',
-            'id_ID' : 'Bytes'
+  	    'ms_MY' : 'Bytes',
+	    'nl_NL' : 'Bytes',
+            'id_ID' : 'Bytes',
+	    'it_IT' : 'Bytes'
         },
 	'Kilobytes' : {
             'af_ZA' : 'Kilogrepe',
             'fr_FR' : 'Kilo-octets',
-  	  	    'ms_MY' : 'Kilobytes',
-		    'nl_NL' : 'Kilobytes',
-            'id_ID' : 'Kilobytes'
+  	    'ms_MY' : 'Kilobytes',
+	    'nl_NL' : 'Kilobytes',
+            'id_ID' : 'Kilobytes',
+	    'it_IT' : 'Kilobytes'
         },
        'Megabytes' : {
             'af_ZA' : 'Megagrepe',
             'fr_FR' : 'Méga-octets',
-  	  	    'ms_MY' : 'Megabytes',
-		    'nl_NL' : 'Megabytes',
-            'id_ID' : 'Megabytes'
+  	    'ms_MY' : 'Megabytes',
+	    'nl_NL' : 'Megabytes',
+            'id_ID' : 'Megabytes',
+	    'it_IT' : 'Megabytes'
         },
     	'Gigabytes' : {
             'af_ZA' : 'Gigagrepe',
             'fr_FR' : 'Gigaoctets',
-  	  	    'ms_MY' : 'Gigabytes',
-		    'nl_NL' : 'Gigabytes',
-            'id_ID' : 'Gigabytes'
+  	    'ms_MY' : 'Gigabytes',
+	    'nl_NL' : 'Gigabytes',
+            'id_ID' : 'Gigabytes',
+	    'it_IT' : 'Gigabytes'
         },
         'English' : {
             'af_ZA' : 'Engels',
             'fr_FR' : 'English',
             'ms_MY' : 'English',
             'nl_NL' : 'Engels',
-            'id_ID' : 'Inggres'
+            'id_ID' : 'Inggres',
+	    'it_IT' : 'Inglese'
         },
         'Captive Portal' : {
             'af_ZA' : 'Gehokte-portaal',
             'fr_FR' : 'Captive Portal',
             'ms_MY' : 'Captive Portal',
             'nl_NL' : 'Captive Portal',
-            'id_ID' : 'Captive Portal'
+            'id_ID' : 'Captive Portal',
+	    'it_IT' : 'Captive Portal'
         },
-		'Welcome' : {
+        'Welcome' : {
             'af_ZA' : 'Welkom',
             'fr_FR' : 'Welcome',
             'ms_MY' : 'Welcome',
             'nl_NL' : 'Welcome',
-            'id_ID' : 'Welcome'
+            'id_ID' : 'Welcome',
+	    'it_IT' : 'Benvenuto'
         },
-		'IP address' : {
+	'IP address' : {
             'af_ZA' : 'IP-adres',
             'fr_FR' : 'Welcome',
             'ms_MY' : 'Welcome',
             'nl_NL' : 'Welcome',
-            'id_ID' : 'Welcome'
+            'id_ID' : 'Welcome',
+	    'it_IT' : 'Indirizzo IP'
         },
-		'connected / left' : {
+	'connected / left' : {
             'af_ZA' : 'gekoppel / beskikbaar',
             'fr_FR' : 'connected / left',
             'ms_MY' : 'connected / left',
             'nl_NL' : 'gekoppel / beskikbaar',
-            'id_ID' : 'connected / left'
+            'id_ID' : 'connected / left',
+	    'it_IT' : 'connesso / rimanenza'
         },
-		'connected'  : {
+	'connected'  : {
             'af_ZA' : 'Gekoppel',
             'fr_FR' : 'connected',
             'ms_MY' : 'connected',
             'nl_NL' : 'connected',
-            'id_ID' : 'connected'
+            'id_ID' : 'connected',
+	    'it_IT' : 'Connezzo'
         },
-		'status refresh'  : {
+	'status refresh'  : {
             'af_ZA' : 'Verkry nuutste elke',
             'fr_FR' : 'status refresh',
             'ms_MY' : 'status refresh',
             'nl_NL' : 'status refresh',
-            'id_ID' : 'status refresh'
+            'id_ID' : 'status refresh',
+	    'it_IT' : 'rinfresca stato'
         },
-		'log off' : {
+	'log off' : {
             'af_ZA' : 'Teken af',
             'fr_FR' : 'log off',
             'ms_MY' : 'log off',
             'nl_NL' : 'log off',
-            'id_ID' : 'log off'
+            'id_ID' : 'log off',
+	    'it_IT' : 'disconnessione'
         },
-		'bytes up/down': {
+	'bytes up/down': {
             'af_ZA' : 'Grepe op-/afgelaai',
             'fr_FR' : 'bytes up/down',
             'ms_MY' : 'bytes up/down',
             'nl_NL' : 'Grepe op-/afgelaai',
-            'id_ID' : 'bytes up/down'
+            'id_ID' : 'bytes up/down',
+	    'it_IT' : 'bytes ricev/inv'
         }
     };
 
