d5070df9ccdcd170f476834f7a703cb6db86ca0c
Oleg Dolya <oleg.dolya@gmail.com>
2013-08-17 15:31:28 +0300

    added new flash banner

--- auto.php
+++ auto.php
@@ -203,7 +203,39 @@ function loadAction(){
   
 </div>-->
 <div class="pure-u-1 visible-desktop" id="main-desktop">
-  <a onclick="submitADS();"><img src="static/img/r.jpg"></a>
+  <a onclick="submitADS();"><object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="960" height="450" id="960x450_BBMedia" align="middle">
+                                <param name="movie" value="static/flash/bbm960x450.swf" />
+                                <param name="quality" value="high" />
+                                <param name="bgcolor" value="#ffffff" />
+                                <param name="play" value="true" />
+                                <param name="loop" value="true" />
+                                <param name="wmode" value="window" />
+                                <param name="scale" value="showall" />
+                                <param name="menu" value="true" />
+                                <param name="devicefont" value="false" />
+                                <param name="salign" value="" />
+                                <param name="allowScriptAccess" value="sameDomain" />
+                                <!--[if !IE]>-->
+                                <object type="application/x-shockwave-flash" data="static/flash/bbm960x450.swf" width="960" height="450">
+                                        <param name="movie" value="static/flash/bbm960x450.swf" />
+                                        <param name="quality" value="high" />
+                                        <param name="bgcolor" value="#ffffff" />
+                                        <param name="play" value="true" />
+                                        <param name="loop" value="true" />
+                                        <param name="wmode" value="window" />
+                                        <param name="scale" value="showall" />
+                                        <param name="menu" value="true" />
+                                        <param name="devicefont" value="false" />
+                                        <param name="salign" value="" />
+                                        <param name="allowScriptAccess" value="sameDomain" />
+                                <!--<![endif]-->
+                                        <a href="http://www.adobe.com/go/getflash">
+                                                <img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Get Adobe Flash player" />
+                                        </a>
+                                <!--[if !IE]>-->
+                                </object>
+                                <!--<![endif]-->
+                        </object></a>
 </div>
 <div class="pure-u-1"><div  id="footer"><button class="pure-button pure-input-1-1 pure-button-primary" onclick="submitInternet();">Internet</button></div></div>
 </div>
